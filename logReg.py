a9915e890d1c4e017e33b820fe325310c6ca8d33
Stephen Minter <sminter@cryomagnetics>
2024-01-09 14:41:48 -0500

    1/9/2024 update

--- logReg.py
+++ logReg.py
@@ -30,6 +30,39 @@ def runLogReg(X, y, degree, train_size, count_plot=True, Cs=10, cv=5, random_sta
 	
 	return log_model, confusionMatrix, accuracyScore
 	
+def runKNeighbors(X, y, train_size, degree=1, k_neighbors=list(range(1,20)), showResPlot = True, cv=5, random_state=101):
+
+	from regressionSetup import poly_TTS_scale
+	from sklearn.neighbors import KNeighborsClassifier
+	from sklearn.model_selection import GridSearchCV
+	
+	X_train, X_test, y_train, y_test = poly_TTS_scale(X=X,y=y,degree=degree,train_size=train_size,random_state=random_state)
+	
+	param_grid = {'n_neighbors':k_neighbors}
+	
+	grid_model = GridSearchCV(estimator=KNeighborsClassifier(),param_grid=param_grid,cv=cv,scoring='accuracy').fit(X_train,y_train)
+	
+	confusionMatrix, accuracyScore= modelEvaluation(y_test,grid_model.predict(X_test))
+	
+	if len(k_neighbors)>1 and showResPlot:
+		import matplotlib.pyplot as plt
+		plt.plot(k_neighbors,grid_model.cv_results_['mean_test_score'],'k.')
+	
+	return grid_model, confusionMatrix, accuracyScore
+	
+def runSVMClassifier(X, y, train_size, degree=1, param_grid = {'C':[0.01,0.1,1],'kernel':['linear','rbf']}, random_state=101):
+	
+	from sklearn.svm import SVC
+	from sklearn.model_selection import GridSearchCV
+	from regressionSetup import poly_TTS_scale
+	
+	X_train, X_test, y_train, y_test = poly_TTS_scale(X=X,y=y,degree=degree,train_size=train_size,random_state=random_state)
+	
+	SVM_model = GridSearchCV(SVC(),param_grid).fit(X_train,y_train)
+	# SVC parameters: kernel:['linear','poly' (+'degree'), 'rbf', 'sigmoid', 'precomputed'], C (regularization strength proportional to 1/C, L2 penalty), gamma: (kernel coefficient for 'rbf', 'poly', or 'sigmoid')
+	
+	return SVM_model	
+	
 def modelEvaluation(y_test,y_pred):
 
 	from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
